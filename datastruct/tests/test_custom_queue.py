c195d660f92d675cb4ed6dd83d640a18bb7353af
Maria <mararyabina62@gmail.com>
2023-03-12 17:43:11 +1000

    Добавили тест dequeue

--- datastruct/tests/test_custom_queue.py
+++ datastruct/tests/test_custom_queue.py
@@ -2,16 +2,24 @@ import unittest
 from datastruct.datastruct.custom_queue import Queue
 
 
-queue = Queue()
-queue.enqueue('data1')
-queue.enqueue('data2')
-queue.enqueue('data3')
-
-
 class Testutils(unittest.TestCase):
 
     def test_enqueue(self):
         """
         Тест enqueue
         """
+        queue = Queue()
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
         self.assertEqual(queue.head.data, "data1")
+
+    def test_dequeue(self):
+        """
+        Тест enqueue
+        """
+        queue1 = Queue()
+        queue1.enqueue('data1')
+        self.assertEqual(queue1.dequeue(), "data1")
+        self.assertEqual(queue1.dequeue(), None)
+
