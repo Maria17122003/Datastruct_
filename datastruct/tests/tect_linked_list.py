a6d0328977aa4a6d09f3865f16b3612d55390440
Maria17122003 <123393865+Maria17122003@users.noreply.github.com>
2023-03-26 00:05:16 +1000

    Update tect_linked_list.py

--- datastruct/tests/tect_linked_list.py
+++ datastruct/tests/tect_linked_list.py
@@ -30,3 +30,30 @@ class Testutils(unittest.TestCase):
         ll.insert_at_end({'id': 3})
         ll.insert_beginning({'id': 0})
         self.assertEqual(ll.print_ll(), print("{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"))
+        
+    def test_to_list(self):
+        """
+        Тест
+        to_list
+        """
+        self.ll_1 = LinkedList()
+        self.ll_1.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        self.ll_1.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        self.ll_1.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        self.ll_1.insert_beginning({'id': 0, 'username': 'serebro'})
+        self.assertEqual(self.ll_1.to_list(), [{'id': 0, 'username': 'serebro'}, {'id': 1, 'username': 'lazzy508509'},
+                                               {'id': 2, 'username': 'mik.roz'}, {'id': 3, 'username': 'mosh_s'}])
+
+    def test_get_data_by_id(self):
+        """
+        Тест
+        get_data_by_id
+        """
+        self.ll2 = LinkedList()
+        self.ll2.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        self.ll2.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        self.ll2.insert_at_end({'id': 3, 'username': 'mosh_s'})
+        self.ll2.insert_beginning({'id': 0, 'username': 'serebro'})
+        self.assertEqual(self.ll2.get_data_by_id(3), {'id': 3, 'username': 'mosh_s'})
+        self.assertEqual(self.ll2.get_data_by_id(4), 'Данные не являются словарем или в словаре нет id')
+
